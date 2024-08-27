24c560a8cb6c986eb422cc2a8c32e966bbe0f257
justArale <just1arale@gmail.com>
2024-08-27 21:21:28 +0200

    added rollup config file

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      exports: "named",
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+  ],
+  external: ["react", "react-dom"],
+};
