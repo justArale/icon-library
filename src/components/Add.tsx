c15c45f4d3a568e7e58c9f253bcab14b2cd47fc1
justArale <just1arale@gmail.com>
2024-08-27 21:59:43 +0200

    changed the box-fill to none and the fill in the path of the icon to currentColor

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -5,7 +5,7 @@ const Add: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
   return (
     <SvgIcon
       viewBox="0 0 24 24"
-      fill="currentColor"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     />
