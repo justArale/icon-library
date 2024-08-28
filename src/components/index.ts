13f1e73eeb5d4ece778d1342b40a986dec308ac7
justArale <just1arale@gmail.com>
2024-08-28 09:28:30 +0200

    add new icon component: Edit, removed edit.svg #2

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as Cancel } from "./Cancel";
 export { default as Check } from "./Check";
 export { default as Clear } from "./Clear";
 export { default as Delete } from "./Delete";
+export { default as Edit } from "./Edit";
