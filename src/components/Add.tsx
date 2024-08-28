765b32d68a9fdc08042cd005a7562eb001bbebef
justArale <just1arale@gmail.com>
2024-08-28 08:48:44 +0200

    clean code: removed import of the svg and set the path directly in the component

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { AddIcon as SvgIcon } from "../svgs";
 
 const Add: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
   return (
-    <SvgIcon
+    <svg
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
-    />
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M10.5 13.5H2.5V10.5H10.5V2.5H13.5V10.5H21.5V13.5H13.5V21.5H10.5V13.5Z"
+        fill="currentColor"
+      />
+    </svg>
   );
 };
 
